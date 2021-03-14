8c6c0e552372f1ade5ab0f5b79b93d801419561c
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-14 01:55:41 -0500

    Wrapper for the response that the action commands will send back to the user

---
+++ src/Model/Response.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Response {
+
+    private String response;
+
+    public Response(String message) {
+        this.response = message;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
