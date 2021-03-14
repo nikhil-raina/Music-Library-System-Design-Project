3a0db7209eac1d48e1c4637540987197b3a2ada1
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-14 05:56:43 -0400

    Completed the Concrete Command Help

--- src/Model/ActionHelp.java
+++ src/Model/ActionHelp.java
@@ -4,19 +4,17 @@ package Model;
 public class ActionHelp implements Request{
     @Override
     public void performRequest() {
-        System.out.println("Reached");
-        /**
-         * commands: "add", "remove", "search", "rate", "create", "show"
-         * mediaType: "song", "artist", "release"
-         * usage:   add  => add <mediaType> in <collection name>
-         *        remove => remove <mediaType> in <collection name>
-         *                  remove <collection name>
-         *        search => search <mediaType> in <collection name>
-         *               => search <mediaType> in database
-         *         rate  => rate <mediaType> (only songs)
-         *        create => create <collection name>
-         *         show  => show <collection name>
-         */
+        System.out.println("\tcommands: \"add\", \"remove\", \"search\", \"rate\", \"create\", \"show\" \n" +
+                "\tmediaType: \"song\", \"artist\", \"release\"\n" +
+                "\tusage:\tadd  => add <mediaType> in <collection name> \n" +
+                "\t\t  remove => remove <mediaType> in <collection name> \n" +
+                "\t\t\t\t => remove <collection name> \n" +
+                "\t\t  search => search <mediaType> in <collection name> \n" +
+                "\t\t\t\t => search <mediaType> in database \n" +
+                "\t\t\trate => rate <mediaType> <song name>(only songs) \n" +
+                "\t\t  create => create <collection name> \n" +
+                "\t\t\tshow => show <collection name> \n"
+                );
     }
 
     @Override
