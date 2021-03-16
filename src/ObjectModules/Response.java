cfb164b36e7c05e204f15e508342407e493032dc
nikhil <nxr5013@g.rit.edu>
2021-03-15 23:15:43 -0400

    Created another method to be able to create a response from the result of searching the media.

--- src/ObjectModules/Response.java
+++ src/ObjectModules/Response.java
@@ -1,10 +1,15 @@
 package ObjectModules;
 
-import java.util.ArrayList;
+import java.util.List;
 
+// Wrapper for the string to be shown to the user
 public class Response {
 
-    private final String response;
+    private String response;
+
+    public Response() {
+        this.response = "";
+    }
 
     public Response(String message) {
         this.response = message;
@@ -14,7 +19,19 @@ public class Response {
         return response;
     }
 
-    public String getResponse(ArrayList<Media> media) {
-        return "";
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public void makeResponse(List<LibraryElement> media) {
+        if (media == null) {
+            setResponse("Artist have not been implemented due to lack of data");
+        }
+        StringBuilder response = new StringBuilder("Result...\n");
+        assert media != null;
+        for (LibraryElement e: media) {
+            response.append(e.toString()).append("\n");
+        }
+        setResponse(response.toString());
     }
 }
