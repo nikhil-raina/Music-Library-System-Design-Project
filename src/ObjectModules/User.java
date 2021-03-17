946a0cea5860db81a9e142ed8f9eaabf77deb19d
Nikhil Raina <nikhilrainathegreat@gmail.com>
2021-03-17 18:19:55 -0400

    Removed excess stuff

--- src/ObjectModules/User.java
+++ src/ObjectModules/User.java
@@ -1,29 +1,22 @@
 package ObjectModules;
 
-import Model.Grouping;
-import Model.Request;
-
 public class User {
     public int ID;
     public String userName;
-    public Request request;
-    private MediaCollection collection;
+    public Library library;
 
     // new user
     public User(int ID, String userName) {
         this.ID = ID;
         this.userName = userName;
-        this.collection = new MediaCollection(new Library(), new Grouping());
+        this.library = new Library();
     }
 
     // old user
-    public User(int ID, String userName, Library library) {
-        this.ID = ID;
+    public User(double ID, String userName, Library library) {
+        this.ID = (int) ID;
         this.userName = userName;
-    }
-
-    public MediaCollection getCollection() {
-        return collection;
+        this.library = library;
     }
 
     public String getUserName(){
@@ -35,10 +28,6 @@ public class User {
     }
 
     public Library getLibrary() {
-        return getCollection().getLibrary();
-    }
-
-    public Grouping getDb() {
-        return getCollection().getDb();
+        return this.library;
     }
 }
